8c308b650e7a935b718b4fb1532918551fe0db67
mohammed sayed <mosaeyd4@gmail.com>
2024-03-17 11:13:51 +0200

    m1

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,17 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/accelerate-svgrepo-com (2).svg';
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
-import { HashLink } from 'react-router-hash-link';
+import logo from "../assets/img/accelerate-svgrepo-com (2).svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+import { HashLink } from "react-router-hash-link";
 import { FaGithub } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
 
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export const NavBar = () => {
-
-  const [activeLink, setActiveLink] = useState('home');
+  const [activeLink, setActiveLink] = useState("home");
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
@@ -23,46 +21,86 @@ export const NavBar = () => {
       } else {
         setScrolled(false);
       }
-    }
+    };
 
     window.addEventListener("scroll", onScroll);
 
     return () => window.removeEventListener("scroll", onScroll);
-  }, [])
+  }, []);
 
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
-  }
+  };
 
   return (
     <Router>
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="home">
-            <img src={logo} alt="Logo"/>
+            <img src={logo} alt="Logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === "home" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("home")}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("skills")}
+              >
+                Skills
+              </Nav.Link>
+              <Nav.Link
+                href="#projects"
+                className={
+                  activeLink === "projects"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("projects")}
+              >
+                Projects
+              </Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="https://www.linkedin.com/in/mohamed-sayed02m"><img src={navIcon1} alt="linkend" /></a>
-                <a href="https://www.facebook.com/mohmmaed.sayed?mibextid=2JQ9oc"><img src={navIcon2} alt="" /></a>
-                <a href="https://www.instagram.com/zp_rg"><img src={navIcon3} alt="" /></a>
-                <a href="https://github.com/mosayed4"> <FaGithub  color="white"/></a>
+                <a href="https://www.linkedin.com/in/mohamed-sayed02m">
+                  <img src={navIcon1} alt="linkend" />
+                </a>
+                <a href="https://www.facebook.com/mohmmaed.sayed?mibextid=2JQ9oc">
+                  <img src={navIcon2} alt="" />
+                </a>
+                <a href="https://www.instagram.com/zp_rg">
+                  <img src={navIcon3} alt="" />
+                </a>
+                <a href="https://github.com/mosayed4">
+                  {" "}
+                  <FaGithub color="white" />
+                </a>
+                <a href="https://wa.me/+201150209859" class="whatsapp_float">
+                  <FaWhatsapp color="white" />
+                </a>
               </div>
-              <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span>
-                </button>
+
+              <HashLink to="#" className="vvd">
+                <span></span>
               </HashLink>
-              <a href='https://drive.google.com/file/d/1-ilicuSalLySLkR295yb28iPvmJQvovA/view?usp=drive_link'>
-                <button className="vvd"><span> GO To See CV </span>
+
+              <a href="https://drive.google.com/file/d/1-ilicuSalLySLkR295yb28iPvmJQvovA/view">
+                <button className="vvd">
+                  <span> GO To See CV </span>
                 </button>
               </a>
             </span>
@@ -70,5 +108,5 @@ export const NavBar = () => {
         </Container>
       </Navbar>
     </Router>
-  )
-}
+  );
+};
